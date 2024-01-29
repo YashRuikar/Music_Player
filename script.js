943ce342c924d2c3977bc5882e6c47754afbe250
YashRuikar <yashruikar04@gmail.com>
2024-01-29 19:33:14 +0530

    Seek bar

--- script.js
+++ script.js
@@ -194,3 +194,44 @@ Array.from(document.getElementsByClassName("playlistPlay")).forEach((e) => {
     el.target.classList.add("bi-pause-circle-fill");
   });
 });
+
+// Seek Bar
+
+let currentStart = document.getElementById("currentStart");
+let currentEnd = document.getElementById("currentEnd");
+let seek = document.getElementById("seek");
+let bar2 = document.getElementById("bar2");
+let dot = document.getElementById("dot");
+
+music.addEventListener("timeupdate", () => {
+  let music_curr = music.currentTime;
+  let music_dur = music.duration;
+
+  let min1 = Math.floor(music_dur / 60);
+  let sec1 = Math.floor(music_dur % 60);
+
+  if (sec1 < 10) {
+    sec1 = `0${sec1}`;
+  }
+
+  currentEnd.innerText = `${min1}:${sec1}`;
+
+  let min2 = Math.floor(music_curr / 60);
+  let sec2 = Math.floor(music_curr % 60);
+
+  if (sec2 < 10) {
+    sec2 = `0${sec2}`;
+  }
+
+  currentStart.innerText = `${min2}:${sec2}`;
+
+  let progressBar = parseInt((music_curr / music_dur) * 100);
+  seek.value = progressBar;
+  let seekBar = seek.value;
+  bar2.style.width = `${seekBar}%`;
+  dot.style.left = `${seekBar}%`;
+});
+
+seek.addEventListener("input", () => {
+  music.currentTime = (seek.value * music.duration) / 100;
+});
