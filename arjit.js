cc8bd9d976ea1c214c57b12301c6e51a0cefef3e
YashRuikar <yashruikar04@gmail.com>
2024-02-04 11:57:54 +0530

    playlist responsiveness

--- arjit.js
+++ arjit.js
@@ -131,6 +131,49 @@ Array.from(document.getElementsByClassName("songItem")).forEach((e, i) => {
   e.getElementsByTagName("h5")[0].innerHTML = songs[i].songName;
 });
 
+// search data start
+let search_results = document.getElementsByClassName("search_results")[0];
+
+songs.forEach((element) => {
+  const { id, songName, poster } = element;
+  let card = document.createElement("a");
+  card.classList.add("card");
+  card.href = "#" + id;
+  card.innerHTML = `
+  <img src="${poster}" alt="">
+                            <div class="content">
+                                ${songName}
+                            </div>
+  `;
+  search_results.appendChild(card);
+});
+
+let input = document.getElementsByTagName("input")[0];
+
+input.addEventListener("keyup", () => {
+  let input_value = input.value.toUpperCase();
+  let items = search_results.getElementsByTagName("a");
+
+  for (let index = 0; index < items.length; index++) {
+    let as = items[index].getElementsByClassName("content")[0];
+    let text_value = as.textContent || as.innerHTML;
+
+    if (text_value.toUpperCase().indexOf(input_value) > -1) {
+      items[index].style.display = "flex";
+    } else {
+      items[index].style.display = "none";
+    }
+
+    if (input.value == 0) {
+      search_results.style.display = "none";
+    } else {
+      search_results.style.display = "";
+    }
+  }
+});
+
+// search data end
+
 let masterPlay = document.getElementById("masterPlay");
 let wave = document.getElementById("wave");
 
@@ -169,13 +212,13 @@ let title = document.getElementById("title");
 Array.from(document.getElementsByClassName("playlistPlay")).forEach((e) => {
   e.addEventListener("click", (el) => {
     index = el.target.id;
-    music.src = `audio/arjit/${index}.mp3`;
-    poster_master_play.src = `img/arjit/${index}.jpg`;
+    music.src = `audio/${index}.mp3`;
+    poster_master_play.src = `img/${index}.jpg`;
     music.play();
     wave.classList.add("active1");
     masterPlay.classList.add("bi-pause-fill");
     masterPlay.classList.remove("bi-play-fill");
-    download_music.href = `audio/arjit/${index}.mp3`;
+    download_music.href = `audio/${index}.mp3`;
 
     let songTitles = songs.filter((els) => {
       return els.id == index;
@@ -278,8 +321,8 @@ back.addEventListener("click", () => {
     index = Array.from(document.getElementsByClassName("songItem")).length;
   }
 
-  music.src = `audio/arjit/${index}.mp3`;
-  poster_master_play.src = `img/arjit/${index}.jpg`;
+  music.src = `audio/${index}.mp3`;
+  poster_master_play.src = `img/${index}.jpg`;
   music.play();
   wave.classList.add("active1");
   masterPlay.classList.add("bi-pause-fill");
@@ -309,8 +352,8 @@ next.addEventListener("click", () => {
     index = 1;
   }
 
-  music.src = `audio/arjit/${index}.mp3`;
-  poster_master_play.src = `img/arjit/${index}.jpg`;
+  music.src = `audio/${index}.mp3`;
+  poster_master_play.src = `img/${index}.jpg`;
   music.play();
   wave.classList.add("active1");
   masterPlay.classList.add("bi-pause-fill");
@@ -333,7 +376,6 @@ next.addEventListener("click", () => {
   el.target.classList.remove("bi-play-circle-fill");
   el.target.classList.add("bi-pause-circle-fill");
 });
-
 
 // Shuffle Button
 
@@ -366,32 +408,31 @@ shuffle.addEventListener("click", () => {
   }
 });
 
-
 const next_music = () => {
   if (index == songs.length) {
-    index = 1
+    index = 1;
   } else {
-    index ++
+    index++;
   }
-  
-  music.src = `audio/arjit/${index}.mp3`;
-  poster_master_play.src = `img/arjit/${index}.jpg`;
+
+  music.src = `audio/${index}.mp3`;
+  poster_master_play.src = `img/${index}.jpg`;
   music.play();
   wave.classList.add("active1");
   masterPlay.classList.add("bi-pause-fill");
   masterPlay.classList.remove("bi-play-fill");
-  download_music.href = `audio/arjit/${index}.mp3`;
-  
+  download_music.href = `audio/${index}.mp3`;
+
   let songTitles = songs.filter((els) => {
     return els.id == index;
   });
-  
+
   songTitles.forEach((elss) => {
     let { songName } = elss;
     title.innerHTML = songName;
     download_music.setAttribute("download", songName);
   });
-  
+
   makeAllBackground();
   Array.from(document.getElementsByClassName("songItem"))[
     index - 1
@@ -399,18 +440,17 @@ const next_music = () => {
   makeAllplays();
   el.target.classList.remove("bi-play-circle-fill");
   el.target.classList.add("bi-pause-circle-fill");
-}
-
+};
 
 const repeat_music = () => {
   index;
-  music.src = `audio/arjit/${index}.mp3`;
-  poster_master_play.src = `img/arjit/${index}.jpg`;
+  music.src = `audio/${index}.mp3`;
+  poster_master_play.src = `img/${index}.jpg`;
   music.play();
   wave.classList.add("active1");
   masterPlay.classList.add("bi-pause-fill");
   masterPlay.classList.remove("bi-play-fill");
-  download_music.href = `audio/arjit/${index}.mp3`;
+  download_music.href = `audio/${index}.mp3`;
 
   let songTitles = songs.filter((els) => {
     return els.id == index;
@@ -429,22 +469,21 @@ const repeat_music = () => {
   makeAllplays();
   el.target.classList.remove("bi-play-circle-fill");
   el.target.classList.add("bi-pause-circle-fill");
-}
-
+};
 
 const random_music = () => {
   if (index == songs.length) {
-    index = 1
+    index = 1;
   } else {
-    index = Math.floor((Math.random() * songs.length) + 1)
+    index = Math.floor(Math.random() * songs.length + 1);
   }
-  music.src = `audio/arjit/${index}.mp3`;
-  poster_master_play.src = `img/arjit/${index}.jpg`;
+  music.src = `audio/${index}.mp3`;
+  poster_master_play.src = `img/${index}.jpg`;
   music.play();
   wave.classList.add("active1");
   masterPlay.classList.add("bi-pause-fill");
   masterPlay.classList.remove("bi-play-fill");
-  download_music.href = `audio/arjit/${index}.mp3`;
+  download_music.href = `audio/${index}.mp3`;
 
   let songTitles = songs.filter((els) => {
     return els.id == index;
@@ -463,36 +502,80 @@ const random_music = () => {
   makeAllplays();
   el.target.classList.remove("bi-play-circle-fill");
   el.target.classList.add("bi-pause-circle-fill");
-}
-
+};
 
 music.addEventListener("ended", () => {
-  let b = shuffle.innerHTML
+  let b = shuffle.innerHTML;
 
   switch (b) {
-    case 'next':
-      next_music()
+    case "next":
+      next_music();
       break;
-    
-    case 'repeat':
-      repeat_music()
+
+    case "repeat":
+      repeat_music();
       break;
-    
-    case 'random':
-      random_music()
+
+    case "random":
+      random_music();
       break;
   }
 });
 
 
+let menu_list_active_button = document.getElementById('menu_list_active_button');
+let menu_side = document.getElementsByClassName('menu_side')[0];
+let song_side = document.getElementsByClassName('song_side')[0];
+let masterPlay_side = document.getElementsByClassName('master_play')[0];
 
+function hideMenu() {
+  menu_side.style.transform = "translateX(-100%)";
+  menu_list_active_button.style.opacity = 1;
+}
 
+// Add click event listener to menu_list_active_button
+menu_list_active_button.addEventListener('click', () => {
+  menu_side.style.transform = "unset";
+  menu_list_active_button.style.opacity = 0;
+});
 
+masterPlay_side.addEventListener('click', () => {
+  masterPlay.style.transform = "unset";
+  masterPlay_side.style.opacity = 1;
+});
 
+// Check if the screen width is less than or equal to 930px
+function isMobileScreen() {
+  return window.innerWidth <= 930;
+}
 
+// Function to handle song_side click
+function handleSongSideClick() {
+  if (isMobileScreen()) {
+    hideMenu();
+  }
+}
 
+// Add click event listener to song_side based on the initial screen width
+if (isMobileScreen()) {
+  song_side.addEventListener('click', handleSongSideClick);
+}
 
+// Listen for resize events to dynamically add/remove the event listener based on screen width
+window.addEventListener('resize', () => {
+  if (isMobileScreen()) {
+    song_side.addEventListener('click', handleSongSideClick);
+  } else {
+    song_side.removeEventListener('click', handleSongSideClick);
+  }
+});
 
+// Add click event listener to document to hide menu when clicking anywhere on the page
+// document.addEventListener('click', (event) => {
+//   if (!menu_side.contains(event.target) && !song_side.contains(event.target)) {
+//     hideMenu();
+//   }
+// });
 
 
 // Left and right buttons to shuffle songs list on content section
